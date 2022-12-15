ed3272a6753b150412ba04c5a992cd79b5a41800
UKnowNothingJonSn0w <wojcieszako@gmail.com>
2022-12-15 16:31:34 +0100

    changes

--- invoice/src/app/app.module.ts
+++ invoice/src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagesModule } from './pages/pages.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+//import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+//import {MatInputModule} from '@angular/material/input';
+//import { ToasterModule } from 'angular2-toaster';
+//import * as AOS from 'aos';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule,
+   // MatInputModule,
+   //ToasterModule.forRoot(),
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ invoice/src/app/pages/pages.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { PagesComponent } from './pages.component';
+//import { HomeComponent } from './home/home.component';
+import { PagesRoutingModule } from './pages-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BsModalService, BsModalRef, ModalModule } from 'ngx-bootstrap/modal';
+//import {MatInputModule} from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+//import {MatDialogModule} from '@angular/material/dialog';
+//import * as AOS from 'aos';
+
+@NgModule({
+  declarations: [
+    PagesComponent,
+   // HomeComponent,
+  ],
+
+  imports: [
+   ModalModule.forRoot(),
+    PagesRoutingModule,
+    BrowserModule,
+    HttpClientModule,
+   // MatInputModule,
+    FormsModule,
+    //MatDialogModule,
+  ],
+
+  providers: [],
+  bootstrap: [PagesComponent],
+})
+export class PagesModule {}
